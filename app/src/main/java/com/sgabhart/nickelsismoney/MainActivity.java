ce37076a5041bc895776ea67623817b702b8515c
Spencer Gabhart <sgabhart2230@gmail.com>
2017-09-08 08:19:50 -0400

    DataActivity screen accessibility

--- app/src/main/java/com/sgabhart/nickelsismoney/MainActivity.java
+++ app/src/main/java/com/sgabhart/nickelsismoney/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sgabhart.nickelsismoney;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void modifyData(View v){
+        Intent myIntent = new Intent(this, DataActivity.class);
+        this.startActivity(myIntent);
     }
 }
