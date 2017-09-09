0485e433d273288bdca93f55b918dabe7e1c2711
Spencer Gabhart <sgabhart2230@gmail.com>
2017-09-09 12:38:47 -0400

    Groundwork for sharing between activities

--- app/src/main/java/com/sgabhart/nickelsismoney/MainActivity.java
+++ app/src/main/java/com/sgabhart/nickelsismoney/MainActivity.java
@@ -3,50 +3,39 @@ package com.sgabhart.nickelsismoney;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String MA = "MainActivity";
+    public static Mortgage mortgage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mortgage = new Mortgage();
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStart(){
+        super.onStart();
+        updateView();
+    }
+
+    public void updateView(){
+        TextView amountTV = (TextView)(findViewById(R.id.amount));
+        amountTV.setText(mortgage.getFormattedAmount());
+        TextView yearsTV = (TextView)(findViewById(R.id.years));
+        yearsTV.setText(mortgage.getYears());
+        TextView rateTV = (TextView)(findViewById(R.id.rate));
+        rateTV.setText(100 * mortgage.getRate() + "%");
+        TextView monthlyTV = (TextView)(findViewById(R.id.payment));
+        monthlyTV.setText(mortgage.formattedMonthly());
+        TextView totalTV = (TextView)(findViewById(R.id.total));
+        totalTV.setText(mortgage.formattedTotal());
     }
 
     public void modifyData(View v){
         Intent myIntent = new Intent(this, DataActivity.class);
         this.startActivity(myIntent);
-    }
-
-    public void onStart(){
-        super.onStart();
-        Log.w(MA, "Inside onStart\n");
-    }
-
-    public void onResume(){
-        super.onResume();
-        Log.w(MA, "Inside onResume\n");
-    }
-
-    public void onRestart(){
-        super.onRestart();
-        Log.w(MA, "Inside onRestart\n");
-    }
-
-    public void onPause(){
-        super.onPause();
-        Log.w(MA, "Inside onPause\n");
-    }
-
-    public void onStop(){
-        super.onStop();
-        Log.w(MA, "Inside onStop\n");
-    }
-
-    public void onDestroy(){
-        super.onDestroy();
-        Log.w(MA, "Inside onDestroy\n");
     }
 }
