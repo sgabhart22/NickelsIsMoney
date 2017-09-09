4ac037542a7f8a9d95d8680b9a55482e3c10210f
Spencer Gabhart <sgabhart2230@gmail.com>
2017-09-09 12:16:37 -0400

    Added logging for DataActivity life cycle methods

--- app/src/main/java/com/sgabhart/nickelsismoney/DataActivity.java
+++ app/src/main/java/com/sgabhart/nickelsismoney/DataActivity.java
@@ -6,9 +6,12 @@ package com.sgabhart.nickelsismoney;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class DataActivity extends AppCompatActivity {
+    public static final String DA = "DataActivity";
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
@@ -16,5 +19,35 @@ public class DataActivity extends AppCompatActivity {
 
     public void goBack(View v){
         this.finish();
+    }
+
+    public void onStart(){
+        super.onStart();
+        Log.w(DA, "Inside onStart\n");
+    }
+
+    public void onResume(){
+        super.onResume();
+        Log.w(DA, "Inside onResume\n");
+    }
+
+    public void onRestart(){
+        super.onRestart();
+        Log.w(DA, "Inside onRestart\n");
+    }
+
+    public void onPause(){
+        super.onPause();
+        Log.w(DA, "Inside onPause\n");
+    }
+
+    public void onStop(){
+        super.onStop();
+        Log.w(DA, "Inside onStop\n");
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        Log.w(DA, "Inside onDestroy\n");
     }
 }
